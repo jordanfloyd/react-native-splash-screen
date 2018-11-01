01de0cf69590b1354fae0efa0000221478b5f16d
Jordan Floyd <jfloyd@ad60.com>
2018-11-01 15:59:48 -0400

    iOS: make the delay longer.

--- ios/RNSplashScreen.m
+++ ios/RNSplashScreen.m
@@ -51,7 +51,7 @@ RCT_EXPORT_MODULE(SplashScreen)
                            waiting = false;
                        });
     } else {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [loadingView removeFromSuperview];
         });
     }
